db3f0c7595a8c23abf907a7e6eb23e5ba2e33693
Liban Mohamed <39710889+jabsadheer101@users.noreply.github.com>
2018-07-01 14:16:45 -0400

    Update makemoney.py

--- makemoney.py
+++ makemoney.py
@@ -4,7 +4,7 @@ from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load the dataset
-prices = datasets.load_boston()
+prices = datasets.load_chase()
 
 # Use only one feature
 prices_X = diabetes.data[:, np.newaxis, 2]
@@ -18,7 +18,7 @@ prices_y_train = prices.target[:-20]
 prices_y_test = prices.target[-20:]
 
 # Create linear regression object
-regr = linear_model.SupportVectorMachine()
+regr = linear_model.SupportVectorMachine() #you can just as easily replace this linear_model.whichever_algorithm_you_prefer
 
 # Train the model using the training sets
 regr.fit(prices_X_train, prices_y_train)
@@ -26,6 +26,7 @@ regr.fit(prices_X_train, prices_y_train)
 # Make predictions using the testing set
 prices_y_pred = regr.predict(prices_X_test)
 
+#Using LSTM-Model
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn import datasets,
@@ -50,15 +51,3 @@ model.fit(X, Y, epochs=150, batch_size=10)
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-import gym
-env = gym.make('sairen-v0')
-for i_episode in xrange(20):
-    observation = env.reset()
-    for t in xrange(100):
-        env.render()
-        print observation
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print "Episode finished after {} timesteps".format(t+1)
-            break
